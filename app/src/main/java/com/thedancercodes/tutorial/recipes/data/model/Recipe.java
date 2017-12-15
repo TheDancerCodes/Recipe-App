340cdbdc80a4daafeb11a9df6a9c634b97b5e3ce
TheDancerCodes <rojtaracha@gmail.com>
2017-12-15 18:21:54 +0300

    [FEATURE] Create Recipe Data Model: [+] Start by building the app from the smallest units first -> The Recipe [+] Using InputStream because to read things out of the asset folder we will use an input stream.

---
+++ app/src/main/java/com/thedancercodes/tutorial/recipes/data/model/Recipe.java
@@ -0,0 +1,38 @@
+package com.thedancercodes.tutorial.recipes.data.model;
+
+import java.io.InputStream;
+
+/**
+ * Created by nabwera on 15/12/2017.
+ */
+
+public class Recipe {
+
+    public final String id;
+    public final String title;
+    public final String description;
+
+
+    // The constructor should be private because we are making a static factory method to parse
+    // the recipe and create it.
+    private Recipe(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+
+    /**
+     * The static method is `readFromStream`
+     *
+     *
+     * IDEA:
+     *   1) Read from the input stream,
+     *   2) Parse all the text,
+     *   3) Assign them into fields: id, title and description
+     *   4) Create a Recipe with the private constructor
+      */
+    public Recipe readFromStream(InputStream stream) {
+        return null;
+    }
+}
