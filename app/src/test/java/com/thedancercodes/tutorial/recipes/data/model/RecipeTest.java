67c15207f5510aeba4f7b932c0832755261b0540
TheDancerCodes <rojtaracha@gmail.com>
2017-12-18 13:23:57 +0300

    [FEATURE] Code Coverage: [+] Using Code Coverage to reveal test cases we haven't handled (e.g.)     [=] We haven't tested for a multi-line description.     [=] We do not have a test case for an IO Exception. [+] Implement a test method for the multi-line description test case
    [+] NOTE: 100% Code Coverage DOES NOT mean that your code is 100% CORRECT!

--- app/src/test/java/com/thedancercodes/tutorial/recipes/data/model/RecipeTest.java
+++ app/src/test/java/com/thedancercodes/tutorial/recipes/data/model/RecipeTest.java
@@ -21,10 +21,30 @@ public class RecipeTest {
         // Assert that the recipe returned is not null.
         assertNotNull(recipe);
 
-        // Assert that the id is what we are expecting [water]
+        // Assert that the id, title & description is what we are expecting
         assertEquals("water", recipe.id);
         assertEquals("Water", recipe.title);
         assertEquals("Put glass under tap. Open tap. Close tap. Drink.", recipe.description);
+    }
+
+    /*
+      A test method for the multi-line description test case.
+     */
+    @Test
+    public void mixed() {
+        InputStream stream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
+        Recipe recipe = Recipe.readFromStream(stream);
+        assertNotNull(recipe);
+        assertEquals("punch", recipe.id);
+        assertEquals("Punch", recipe.title);
+        assertEquals(
+                "Juice of 3 lemons\n" +
+                "1 orange\n" +
+                "1 pint grape juice\n" +
+                "1 cup sugar\n" +
+                "1 cup water\n" +
+                "1 pine apple juice\n" +
+                "Mix all together and strain. Add large piece of ice.", recipe.description);
     }
 
 }
