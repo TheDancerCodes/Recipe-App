9ab31ca3dbaf252560f16e1bd6ecdcc036c38749
TheDancerCodes <rojtaracha@gmail.com>
2017-12-19 22:47:29 +0300

    [FEATURE] Add Count Test: [+] A test that verifies the count of the number of recipes in the assets folder.

--- app/src/androidTest/java/com/thedancercodes/tutorial/recipes/data/local/RecipeStoreTest.java
+++ app/src/androidTest/java/com/thedancercodes/tutorial/recipes/data/local/RecipeStoreTest.java
@@ -11,17 +11,17 @@ import static org.junit.Assert.*;
  * Created by TheDancerCodes on 18/12/2017.
  */
 public class RecipeStoreTest {
+
+    /**
+     * GOAL:
+     * We want to return a store that is not null, and contains no recipe.
+     */
     @Test
     public void nullDirectory() {
         Context context = InstrumentationRegistry.getTargetContext();
 
         // Create a recipe store with the context and null as the directory.
         RecipeStore store = new RecipeStore(context, null);
-
-        /**
-         * GOAL:
-         * We want to return a store that is not null, and contains no recipe.
-         */
 
         // Asserting that the store is not null
         assertNotNull(store);
@@ -31,5 +31,28 @@ public class RecipeStoreTest {
 
         // Asserting that the recipe store is empty
         assertEquals(0, store.recipes.size());
+    }
+
+
+    /**
+     * GOAL:
+     * We want to return a store that is not null, and
+     * Verify the count of the number of recipes in the assets folder.
+     */
+    @Test
+    public void count() {
+        Context context = InstrumentationRegistry.getTargetContext();
+
+        // Create a recipe store with the context and recipes as the directory.
+        RecipeStore store = new RecipeStore(context, "recipes");
+
+        // Asserting that the store is not null
+        assertNotNull(store);
+
+        // Asserting that the recipes are not null
+        assertNotNull(store.recipes);
+
+        // Asserting that the recipe store has 4 recipes
+        assertEquals(4, store.recipes.size());
     }
 }
