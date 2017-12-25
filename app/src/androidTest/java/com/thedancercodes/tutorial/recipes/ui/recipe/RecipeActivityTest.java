61eb2bb62b5955ecf20c5de17ed1d44e7b1df579
TheDancerCodes <rojtaracha@gmail.com>
2017-12-26 01:48:07 +0300

    [FEATURE] An Espresso test for RecipeActivityTest: [+] Test Case 1: Launching the activity without a proper recipe ID.     [=] Launch an activity without an intent.     [=] Check that is shows 'recipe not found'     [=] Check that the title is not displayed
    [+] TestCase 2: Verify that when we click on the title, it saves it as favorite.
        [=] To load a valid recipe, pass an intent to Activity Test Rule
        [=] Verify that title view starts off with not selected, and changes to selected when you click on it.

---
+++ app/src/androidTest/java/com/thedancercodes/tutorial/recipes/ui/recipe/RecipeActivityTest.java
@@ -0,0 +1,60 @@
+package com.thedancercodes.tutorial.recipes.ui.recipe;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+
+import com.thedancercodes.tutorial.recipes.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isSelected;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+/**
+ * Created by TheDancerCodes on 26/12/2017.
+ */
+public class RecipeActivityTest {
+
+    @Rule
+    public ActivityTestRule<RecipeActivity> activityRule
+            = new ActivityTestRule<>(RecipeActivity.class, true, false);
+
+    @Test
+    public void recipeNotFound() {
+        // Launch an activity without an intent
+        activityRule.launchActivity(null);
+
+        // Check that it shows 'recipe not found'
+        onView(withId(R.id.description))
+                .check(matches(withText(R.string.recipe_not_found)));
+
+        // Check that that the title is not displayed
+        onView(withId(R.id.title))
+                .check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void clickToFavorite() {
+        // To load a valid recipe, pass an intent to the ActivityRule
+        Intent intent = new Intent();
+        intent.putExtra(RecipeActivity.KEY_ID, "creamed_carrots");
+        activityRule.launchActivity(intent);
+
+        // Verify that title view starts off with not selected, and
+        // changes to selected when you click on it.
+        onView(withId(R.id.title))
+                .check(matches(withText("Creamed Carrots")))
+                .check(matches(not(isSelected())))
+                .perform(click())
+                .check(matches(isSelected()));
+    }
+
+}
