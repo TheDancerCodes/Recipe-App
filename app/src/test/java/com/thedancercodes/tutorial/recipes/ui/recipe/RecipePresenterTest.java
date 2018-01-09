747b15ec8e2eac6cc6ef32df0b6c653de2cd5705
TheDancerCodes <rojtaracha@gmail.com>
2018-01-09 23:21:56 +0300

    [FEATURE] Testing with MVP: recipeNotFound() [+] Create a test and apply the ARRANGE -> ACT -> ASSERT approach.
    [+] Arrange -> Set up; If somebody calls the getRecipe() function, we return a null object.
    [+] Act -> Asking the presenter to do something.
    [+] Assert -> We are verifying the behaviour we expect.

--- app/src/test/java/com/thedancercodes/tutorial/recipes/ui/recipe/RecipePresenterTest.java
+++ app/src/test/java/com/thedancercodes/tutorial/recipes/ui/recipe/RecipePresenterTest.java
@@ -4,6 +4,7 @@ import com.thedancercodes.tutorial.recipes.data.local.Favorites;
 import com.thedancercodes.tutorial.recipes.data.local.RecipeStore;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
@@ -29,5 +30,18 @@ public class RecipePresenterTest {
 
         // Instantiate the Presenter object
         presenter = new RecipePresenter(store, view, favorites);
+    }
+
+    @Test
+    public void recipeNotFound() {
+
+        // ARRANGE -> Return a null object when getRecipe() function is called
+        Mockito.when(store.getRecipe(Mockito.anyString())).thenReturn(null);
+
+        // ACT -> Load the non-existent recipe
+        presenter.loadRecipe("no_such_recipe");
+
+        // ASSERT -> Verify that the showRecipeNotFound() error function is called.
+        Mockito.verify(view, Mockito.times(1)).showRecipeNotFoundError();
     }
 }
