4735ff7ff177b62c803f679cec919a6b61ea7f31
TheDancerCodes <rojtaracha@gmail.com>
2018-01-10 12:21:21 +0300

    [FEATURE] Testing with MVP: ArgumentCaptor [+] PART 1:   [=] Test loading an actual recipe -> public void loadWaterAndFavorite()   [=] From RecipeTest, copy code for Use getResourceAsStream -> to obtain an info stream for recipes/water.txt & Create a Recipe from the stream.
      [=] Apply ASSIGN & ACT:
        * ASSIGN -> Return a recipe object when getRecipe() function is called.
        * ACT -> Load the recipe with id "water"
    
    [+] PART 2:
      [=] Verify the behaviour to favorite this recipe.
      [=] Call toggleFavorite() -> presenter.toggleFavorite();
    
      [=] The initial state is false; when toggleFavorite() is called it should return true, flipping from the default false.
      [=] Mockito.when(favorites.toggle(Mockito.anyString())).thenReturn(true);
    
    [+] PART 3: Verifying the UI.
      [=] Verify that the Presenter calls setFavorite() on the view with the correct variable using an ArgumentCaptor.
      [=] Initialise the captor and then assert this case.
      [=] Mockito.verify(view, Mockito.times(2)).setFavorite(captor.capture());
    
    [+] PART 4: Verify the values captured by the captor.
      [=] assertFalse & assertTrue for the not favorite and favorite state values that were captured.

--- app/src/test/java/com/thedancercodes/tutorial/recipes/ui/recipe/RecipePresenterTest.java
+++ app/src/test/java/com/thedancercodes/tutorial/recipes/ui/recipe/RecipePresenterTest.java
@@ -2,10 +2,14 @@ package com.thedancercodes.tutorial.recipes.ui.recipe;
 
 import com.thedancercodes.tutorial.recipes.data.local.Favorites;
 import com.thedancercodes.tutorial.recipes.data.local.RecipeStore;
+import com.thedancercodes.tutorial.recipes.data.model.Recipe;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+
+import java.io.InputStream;
 
 import static org.junit.Assert.*;
 
@@ -49,5 +53,42 @@ public class RecipePresenterTest {
     @Test(expected = IllegalStateException.class)
     public void toggleWithoutLoad() {
         presenter.toggleFavorite();
+    }
+
+    @Test
+    public void loadWaterAndFavorite() {
+        // Use getResourceAsStream -> to obtain an info stream for recipes/water.txt
+        InputStream stream = RecipePresenterTest.class.getResourceAsStream("/recipes/water.txt");
+
+        // Create a Recipe
+        Recipe recipe = Recipe.readFromStream(stream);
+
+        // ASSIGN -> Return a recipe object when getRecipe() function is called
+        Mockito.when(store.getRecipe(Mockito.anyString())).thenReturn(recipe);
+
+        // ASSIGN -> return true when toggleFavorite() is called, flipping from the default false.
+        Mockito.when(favorites.toggle(Mockito.anyString())).thenReturn(true);
+
+        // ACT -> Load the recipe with id "water"
+        presenter.loadRecipe("water");
+
+        // ACT -> Toggle recipe to a favorite
+        presenter.toggleFavorite();
+
+        // ASSERT -> Verify that Presenter calls setFavorite() on the view with the correct variable
+        ArgumentCaptor<Boolean> captor = ArgumentCaptor.forClass(Boolean.class);
+
+        // ASSERT -> Verify that the view setFavorite() function is called twice and
+        // the values that we call it with is stored in the captor.
+        Mockito.verify(view, Mockito.times(2)).setFavorite(captor.capture());
+
+        // ASSERT -> Verify the values captured by the captor.
+        // We started off as not favorite recipe, so it should be false.
+        assertFalse(captor.getAllValues().get(0));
+
+        // The second value captured by the captor is true because it happens after toggleFavorite()
+        // is called and the Presenter would call setFavorite() with true to update the UI.
+        assertTrue(captor.getAllValues().get(1));
+
     }
 }
